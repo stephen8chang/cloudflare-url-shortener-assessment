e62295436b14498f0fc4c229c6bd604119073562
Stephen Chang <stephen8chang@gmail.com>
2025-02-27 18:06:05 -0800

    Set up database and create short url model

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class ShortURL(Base):
+    __tablename__ = "short_urls"
+
+    id = Column(Integer, primary_key=True, index=True)
+    short_code = Column(String(10), unique=True, nullable=False)
+    long_url = Column(String, nullable=False)
+    visit_count = Column(Integer, default=0)
+    created_at = Column(DateTime, default=func.now())
+    expires_at = Column(DateTime, nullable=True)
